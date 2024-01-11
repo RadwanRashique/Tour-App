4879a3c84ac725d1f0f1f3855ff255b8f67e2e53
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-01-11 16:55:13 +0530

    added readme.md file

--- src/components/TourCard/Tourcards.js
+++ src/components/TourCard/Tourcards.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function Tourcards() {
+function Tourcards({id,image,info,name,price,removeTour}) {
+
+  const [readMore,setReadMore]=useState(false)
   return (
   <>
-<main>
-    
-</main>
+<article>
+  <img src=' ' alt='' />
+  <footer>
+    <div className='tour-info'>
+      <h4>name</h4>
+      <h4 className='tour-price'>price</h4>
+
+    </div>
+    <p>
+      {/* {
+        readMore? info: 
+
+      } */}
+    </p>
+    <button className='delete-btn'  onClick={()=>removeTour(id)} >not interested</button>
+
+  </footer>
+</article>
 
   </>
   )
